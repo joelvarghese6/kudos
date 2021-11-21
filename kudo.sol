804761d6f00c1a88e5093ebbf612a89f38fecfdf
joelvarghese6 <53945072+joelvarghese6@users.noreply.github.com>
2021-11-21 13:37:19 +0530

    Create kudo.sol

---
+++ kudo.sol
@@ -0,0 +1,14 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract Kudos {
+    
+    mapping(address => [kudo]) allkudos;
+    
+    function getKudos(string what, address giver, string comments)
+}
+
+struct kudo {
+    string what:
+    address from:
+    string comments:
+}
