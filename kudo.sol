3896c3d1d8046c24d62cbe3b81e920b1e3badcf7
joelvarghese6 <53945072+joelvarghese6@users.noreply.github.com>
2021-11-21 14:12:42 +0530

    Update kudo.sol

--- kudo.sol
+++ kudo.sol
@@ -2,13 +2,25 @@ pragma solidity >= 0.7.0 < 0.9.0;
 
 contract Kudos {
     
-    mapping(address => [kudo]) allkudos;
+    mapping(address => Kudo[]) allKudos;
     
-    function getKudos(string what, address giver, string comments)
+    function giveKudos(address who, string memory what, address giver, string memory comments) public {
+        Kudo memory kudo = Kudo(what, giver, comments);
+        allKudos[who].push(kudo);
+    }
 }
 
-struct kudo {
-    string what:
-    address from:
-    string comments:
+struct Kudo {
+    string what;
+    address from;
+    string comments;
 }
+
+
+
+
+/*
+gary: 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+andrew: 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+
+*/
